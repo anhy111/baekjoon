2d150651dbe9c7d18c075e7bd98ec517158f5a8a
anhy111 <67471703+anhy111@users.noreply.github.com>
2023-06-26 11:02:29 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int a = scanner.nextInt();
+        final int b = scanner.nextInt();
+
+        System.out.println(a + b);
+    }
+}

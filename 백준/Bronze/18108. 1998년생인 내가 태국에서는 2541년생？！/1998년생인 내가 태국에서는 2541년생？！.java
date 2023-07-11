2d05df545d5b4a4384139201b2f0853b8c19387b
anhy111 <67471703+anhy111@users.noreply.github.com>
2023-07-12 01:42:44 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 212 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt() - 543);
+    }
+}

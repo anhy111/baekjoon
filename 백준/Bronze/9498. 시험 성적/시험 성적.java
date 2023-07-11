e9553c83e6424d426bbb0af9cf0c4d853990839f
anhy111 <67471703+anhy111@users.noreply.github.com>
2023-07-12 02:05:06 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int score = scanner.nextInt();
+        String result = null;
+        if (score >= 90) {
+            result = "A";
+        } else if (score >= 80) {
+            result = "B";
+        } else if (score >= 70) {
+            result = "C";
+        } else if (score >= 60) {
+            result = "D";
+        } else {
+            result = "F";
+        }
+
+        System.out.println(result);
+    }
+}

a35e073563b7cf03cca4e5e047bc9d560e8097fb
anhy111 <67471703+anhy111@users.noreply.github.com>
2023-07-12 01:40:25 +0900

    [Bronze V] Title: 사칙연산, Time: 212 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        final Scanner scanner = new Scanner(System.in);
+        final int a = scanner.nextInt();
+        final int b = scanner.nextInt();
+        final StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(a + b).append("\n");
+        stringBuilder.append(a - b).append("\n");
+        stringBuilder.append(a * b).append("\n");
+        stringBuilder.append(a / b).append("\n");
+        stringBuilder.append(a % b).append("\n");
+        System.out.println(stringBuilder.toString());
+    }
+}

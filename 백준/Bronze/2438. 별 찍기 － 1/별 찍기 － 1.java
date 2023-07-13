77bdb9dc499210a953d42cef4a89d8e7d9ce37c6
anhy111 <67471703+anhy111@users.noreply.github.com>
2023-07-14 01:44:27 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -3,23 +3,24 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        final BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
         final int count = Integer.parseInt(bufferedReader.readLine());
-        final StringBuilder sb = new StringBuilder();
 
         for (int i = 1; i <= count; i++) {
-            printStar(i, sb);
+            printStar(i, bufferedWriter);
             if (i != count) {
-                sb.append("\n");
+                bufferedWriter.append('\n');
             }
         }
-        System.out.println(sb.toString());
+        bufferedWriter.flush();
 
+        bufferedWriter.close();
         bufferedReader.close();
     }
 
-    private static void printStar(int count, StringBuilder sb) {
+    private static void printStar(int count, BufferedWriter sb) throws IOException {
         for (int i = 0; i < count; i++) {
-            sb.append("*");
+            sb.write('*');
         }
     }
 }

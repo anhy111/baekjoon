a630d54fe01363a4d8fe4723dcb59fde0decf0f4
anhy111 <67471703+anhy111@users.noreply.github.com>
2023-07-14 00:17:45 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 564 ms, Memory: 59364 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,20 +1,22 @@
 import java.io.*;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        final int count = Integer.parseInt(bufferedReader.readLine());
+        final StreamTokenizer streamTokenizer = new StreamTokenizer(System.in);
+        streamTokenizer.nextToken();
+        final int count = (int) streamTokenizer.nval;
+        final StringBuilder sb = new StringBuilder();
 
         final BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = 0; i < count; i++) {
-            final StringTokenizer tokenizer = new StringTokenizer(bufferedReader.readLine());
-            final int a = Integer.parseInt(tokenizer.nextToken());
-            final int b = Integer.parseInt(tokenizer.nextToken());
-            bufferedWriter.write(String.valueOf(a + b));
-            bufferedWriter.newLine();
+            streamTokenizer.nextToken();
+            final int a = (int)streamTokenizer.nval;
+            streamTokenizer.nextToken();
+            final int b = (int) streamTokenizer.nval;
+            sb.append(a + b + "\n");
         }
+        bufferedWriter.write(sb.toString());
         bufferedWriter.flush();
     }
 }

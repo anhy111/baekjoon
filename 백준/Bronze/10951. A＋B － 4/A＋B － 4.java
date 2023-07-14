2156c16b229b45c40d5d47365086c9f09fc77934
anhy111 <67471703+anhy111@users.noreply.github.com>
2023-07-15 02:25:36 +0900

    [Bronze V] Title: A+B - 4, Time: 148 ms, Memory: 15768 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -7,7 +7,7 @@ public class Main {
 
         final StringBuilder stringBuilder = new StringBuilder();
         String str;
-        while ((str = bufferedReader.readLine()) != null && !str.equals("")) {
+        while ((str = bufferedReader.readLine()) != null) {
             final String[] split = str.split(" ");
             int a = Integer.parseInt(split[0]);
             int b = Integer.parseInt(split[1]);

25c59e0738095b10b9b3de768b9da38cf0ff9bc5
anhy111 <67471703+anhy111@users.noreply.github.com>
2023-07-15 02:42:38 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        final int max = Integer.parseInt(bufferedReader.readLine());
+
+        final String[] str = bufferedReader.readLine().split(" ");
+
+        int[] arr = new int[max];
+        for (int i = 0; i < max; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+
+        final int searchInt = Integer.parseInt(bufferedReader.readLine());
+
+        int cnt = 0;
+        for (int i = 0; i < max; i++) {
+            cnt += arr[i] == searchInt ? 1 : 0;
+        }
+
+        System.out.print(cnt);
+        bufferedReader.close();
+    }
+}

dd5040557f54db773487c653349654e5e5140f6c
anhy111 <67471703+anhy111@users.noreply.github.com>
2023-07-17 00:51:43 +0900

    [Bronze V] Title: X보다 작은 수, Time: 172 ms, Memory: 16288 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,54 +1,28 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        final int X = readInt();
+        final int N = readInt();
 
-        final String firstStr = bufferedReader.readLine() + "\n";
-        final Index index = new Index();
-        index.setIndex(0);
-
-        final int X = readInt(firstStr, index);
-        final int N = readInt(firstStr, index);
-
-        final String secondStr = bufferedReader.readLine() + "\n";
         final StringBuilder sb = new StringBuilder();
-        index.setIndex(0);
 
-        while (secondStr.charAt(index.getIndex()) != '\n') {
-            final int in = readInt(secondStr, index);
-            if (in < N) {
-                sb.append(in + " ");
+        for (int i = 0; i < X; i++) {
+            int num = readInt();
+            if (num < N) {
+                sb.append(num + " ");
             }
         }
 
         System.out.print(sb.toString());
-        bufferedReader.close();
     }
 
-    private static int readInt(String str, Index index) {
+    private static int readInt() throws IOException {
         int val = 0;
-        int inx = index.getIndex();
-        do {
-            val = val * 10;
-            val += str.charAt(inx++) - '0';
-        } while (str.charAt(inx) != '\n' && str.charAt(inx) != ' ');
-        index.setIndex(inx == str.length() -1 ? inx : inx + 1);
-        return val;
-    }
-
-    static class Index {
-        private int index;
-
-        public int getIndex() {
-            return index;
+        int total = 0;
+        while ((val = System.in.read()) != '\n' && val != ' ') {
+            total = total * 10 + (val - '0');
         }
-
-        public void setIndex(final int index) {
-            this.index = index;
-        }
+        return total;
     }
-
 }

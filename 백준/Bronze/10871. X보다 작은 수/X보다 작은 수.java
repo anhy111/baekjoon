00d4ff4727ca8df4e77ed375ae1c0a5898c8fc4f
anhy111 <67471703+anhy111@users.noreply.github.com>
2023-07-17 01:03:54 +0900

    [Bronze V] Title: X보다 작은 수, Time: 140 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -7,21 +7,22 @@ public class Main {
 
         final StringBuilder sb = new StringBuilder();
 
+        int num;
         for (int i = 0; i < X; i++) {
-            int num = readInt();
+            num = readInt();
             if (num < N) {
                 sb.append(num).append(' ');
             }
         }
 
-        System.out.print(sb.toString());
+        System.out.print(sb);
     }
 
     private static int readInt() throws IOException {
         int val = 0;
         int total = 0;
         while ((val = System.in.read()) != '\n' && val != ' ') {
-            total = total * 10 + (val - 48);
+            total = total * 10 + (val - '0');
         }
         return total;
     }

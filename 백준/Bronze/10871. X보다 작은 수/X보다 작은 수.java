c65d8804a50fa18c2f62affc3d846ed17fa2d450
anhy111 <67471703+anhy111@users.noreply.github.com>
2023-07-17 00:24:54 +0900

    [Bronze V] Title: X보다 작은 수, Time: 188 ms, Memory: 16912 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -6,15 +6,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        final String[] str = bufferedReader.readLine().split(" ");
-        int X = Integer.parseInt(str[1]);
+        final String firstStr = bufferedReader.readLine() + "\n";
+        final Index index = new Index();
+        index.setIndex(0);
 
-        final String[] inputs = bufferedReader.readLine().split(" ");
+        final int X = readInt(firstStr, index);
+        final int N = readInt(firstStr, index);
+
+        final String secondStr = bufferedReader.readLine() + "\n";
         final StringBuilder sb = new StringBuilder();
+        index.setIndex(0);
 
-        for (int i = 0; i < inputs.length; i++) {
-            final int in = Integer.parseInt(inputs[i]);
-            if (in < X) {
+        while (secondStr.charAt(index.getIndex()) != '\n') {
+            final int in = readInt(secondStr, index);
+            if (in < N) {
                 sb.append(in + " ");
             }
         }
@@ -22,4 +27,28 @@ public class Main {
         System.out.print(sb.toString());
         bufferedReader.close();
     }
+
+    private static int readInt(String str, Index index) {
+        int val = 0;
+        int inx = index.getIndex();
+        do {
+            val = val * 10;
+            val += str.charAt(inx++) - '0';
+        } while (str.charAt(inx) != '\n' && str.charAt(inx) != ' ');
+        index.setIndex(inx == str.length() -1 ? inx : inx + 1);
+        return val;
+    }
+
+    static class Index {
+        private int index;
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setIndex(final int index) {
+            this.index = index;
+        }
+    }
+
 }

26254265a45ee884061573b4a5a147ac74e3a338
anhy111 <67471703+anhy111@users.noreply.github.com>
2023-07-17 15:46:35 +0900

    [Bronze V] Title: Hello World, Time: 120 ms, Memory: 13944 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,5 +1,7 @@
-class Main{
-    public static void main(String[] args){
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
     }
 }

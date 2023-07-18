e78dd31bd5115f43f087be10a8b3463b8a2ec10c
anhy111 <67471703+anhy111@users.noreply.github.com>
2023-07-18 21:53:16 +0900

    [Bronze V] Title: 윤년, Time: 128 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,9 +1,8 @@
-import java.util.Scanner;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        final int year = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        final int year = readInt();
         boolean result = false;
         if (year % 400 == 0) {
             result = true;
@@ -12,5 +11,15 @@ public class Main {
         }
 
         System.out.println(result?1:0);
+    }
+
+    private static int readInt() throws IOException {
+        int val = 0;
+        int total = 0;
+
+        while ((val = System.in.read()) != '\n') {
+            total = total * 10 + (val - '0');
+        }
+        return total;
     }
 }

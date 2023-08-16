f2f3abeab21bd6b21063cb1fbff5dcdeb2be5885
anhy111 <67471703+anhy111@users.noreply.github.com>
2023-08-17 02:19:50 +0900

    [Bronze II] Title: 다이얼, Time: 120 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,59 @@
+import java.io.IOException;
+
+enum AlphaToNum {
+    A(2),
+    B(2),
+    C(2),
+    D(3),
+    E(3),
+    F(3),
+    G(4),
+    H(4),
+    I(4),
+    J(5),
+    K(5),
+    L(5),
+    M(6),
+    N(6),
+    O(6),
+    P(7),
+    Q(7),
+    R(7),
+    S(7),
+    T(8),
+    U(8),
+    V(8),
+    W(9),
+    X(9),
+    Y(9),
+    Z(9);
+
+    private int num;
+
+    AlphaToNum(final int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return this.num;
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        int seconds = 0;
+        int val;
+        final AlphaToNum[] values = AlphaToNum.values();
+
+        while ((val = System.in.read()) != '\n') {
+            final int num = AlphaToNum.valueOf(Character.toString(val)).getNum();
+            seconds += num + 1;
+        }
+
+
+        System.out.println(seconds);
+    }
+}

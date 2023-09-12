45a7b8a68772a1558077eb242ba2ada6bf26e0c2
anhy111 <67471703+anhy111@users.noreply.github.com>
2023-09-13 02:18:43 +0900

    [Silver V] Title: 분수찾기, Time: 152 ms, Memory: 16036 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        final int input = readInt();
+
+        int currentLocation = 0;
+        int accumulate = 1;
+        String result;
+
+        while (true) {
+            currentLocation += accumulate;
+
+            if (currentLocation - accumulate < input && input <= currentLocation) {
+                break;
+            }
+            accumulate++;
+        }
+
+        // 시작위치로 되돌리기
+        currentLocation -= accumulate - 1;
+        int left = accumulate;
+        int right = 1;
+
+        if (accumulate % 2 == 0) { // 짝수는 반대방향
+            left = 1;
+            right = accumulate;
+        }
+
+        for (int i = 0; i < accumulate; i++) {
+            if (input == currentLocation) {
+                break;
+            }
+
+            if (accumulate % 2 == 0) { // 짝수는 반대방향
+                left++;
+                right--;
+            } else {
+                left--;
+                right++;
+            }
+
+            currentLocation++;
+        }
+
+        System.out.println(left + "/" + right);
+
+    }
+
+    private static int readInt() throws IOException {
+        int total = 0;
+        int val = 0;
+
+        while ((val = System.in.read()) != '\n' && val != ' ') {
+            total = total * 10 + (val - '0');
+        }
+        return total;
+    }
+}

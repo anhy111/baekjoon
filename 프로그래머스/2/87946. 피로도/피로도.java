3e7b81daf046f4ce5a378c6d74e6f8bcea6e9fb6
anhy111 <67471703+anhy111@users.noreply.github.com>
2024-01-26 11:17:08 +0900

    [level 2] Title: 피로도, Time: 0.07 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,35 +1,24 @@
 class Solution {
+    private boolean[] visits;
+    private int max;
+
     public int solution(int k, int[][] dungeons) {
-        int[] max = new int[1];
-        final boolean[] visits = new boolean[dungeons.length];
-        final int[] array = new int[dungeons.length];
-        dfs(k, dungeons, visits, array, 0, max);
-        return max[0];
+        visits = new boolean[dungeons.length];
+
+        dfs(k, dungeons, 0);
+        return max;
     }
 
-    private void dfs(int k, int[][] dungeons, boolean[] visits, int[] array, int depth, int[] max) {
-        if (depth == dungeons.length) {
-            int cnt = 0;
-            for (int inx : array) {
-                if (dungeons[inx][0] <= k) {
-                    k -= dungeons[inx][1];
-                    cnt++;
-                }
-            }
-
-            if (max[0] < cnt) {
-                max[0] = cnt;
-            }
-        }
-
+    private void dfs(int k, int[][] dungeons, int depth) {
         for (int i = 0; i < dungeons.length; i++) {
-            if (!visits[i]) {
+            if (!visits[i] && k >= dungeons[i][0]) {
                 visits[i] = true;
-                array[depth] = i;
-                dfs(k, dungeons, visits, array, depth + 1, max);
+                dfs(k - dungeons[i][1], dungeons, depth + 1);
 
                 visits[i] = false;
             }
         }
+
+        max = Math.max(depth, max);
     }
 }

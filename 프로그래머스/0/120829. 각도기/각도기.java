57d0cf88fc3e836ec528cfec187d4398a166d769
anhy111 <67471703+anhy111@users.noreply.github.com>
2024-02-09 20:28:57 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int angle) {
+        if (angle == 90) {
+            return 2;
+        } else if (angle == 180) {
+            return 4;
+        }
+        
+        if (angle < 90) {
+            return 1;
+        }
+        return 3;
+    }
+}

73031c055af1e587a5624d82a8c116dca2cd1844
anhy111 <67471703+anhy111@users.noreply.github.com>
2024-02-15 16:38:29 +0900

    [level 3] Title: 등굣길, Time: 0.73 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,35 @@
+
+class Solution {
+
+    public int solution(int m, int n, int[][] puddles) {
+        final int[][] arr = new int[n][m];
+        for (int i = 0; i < puddles.length; i++) {
+            arr[puddles[i][1] - 1][puddles[i][0] - 1] = -1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0) {
+                    arr[0][0] = 1;
+                    continue;
+                }
+                
+                int sum = 0;
+                if (arr[i][j] == -1) {
+                    continue;
+                }
+                
+                if (i > 0 && arr[i - 1][j] != -1) {
+                    sum += arr[i - 1][j];
+                }
+
+                if (j > 0 && arr[i][j - 1] != -1) {
+                    sum += arr[i][j - 1];
+                }
+                arr[i][j] = sum % 1000000007;
+            }
+        }
+
+        return arr[n-1][m-1];
+    }
+}

54f6a20e28492bd1ca4d8d30a03a75ef4859a68a
anhy111 <67471703+anhy111@users.noreply.github.com>
2024-03-05 16:14:32 +0900

    [level 0] Title: 약수 구하기, Time: 2.94 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n) {
+        
+        List<Integer> list = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                list.add(i);
+            }
+        }
+
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}

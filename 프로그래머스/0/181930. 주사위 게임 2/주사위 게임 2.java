6e55022a4633d335b3926998da7acacdcaed26d4
anhy111 <67471703+anhy111@users.noreply.github.com>
2024-04-10 20:44:41 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        if (a == b && b == c) {
+            return (int)((a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)) * (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3)));
+        } else if (a == b || a == c || b == c) {
+            return (int)((a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)));
+        }
+        return a + b + c;
+    }
+}

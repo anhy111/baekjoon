10a1b973347373fa8c01718ea25e5697743c1198
anhy111 <67471703+anhy111@users.noreply.github.com>
2024-04-12 13:45:02 +0900

    [level 0] Title: 조건 문자열, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        if (ineq.equals("<")) {
+            if (eq.equals("=")) {
+                return n <= m ? 1 : 0;
+            }
+            return n < m ? 1 : 0;
+        }
+        
+        if (eq.equals("=")){
+            return n >= m ? 1 : 0;
+        }
+        return n > m ? 1 : 0;
+    }
+}

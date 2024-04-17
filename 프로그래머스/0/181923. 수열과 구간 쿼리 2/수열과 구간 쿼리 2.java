959ade0ad6f580126b3bdab444601268a11f67fb
anhy111 <67471703+anhy111@users.noreply.github.com>
2024-04-17 23:23:08 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 2.20 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -3,12 +3,10 @@ class Solution {
         final int[] answers = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
             int min = Integer.MAX_VALUE;
-            int s = queries[i][0];
-            int e = queries[i][1];
-            int k = queries[i][2];
+            int s = queries[i][0], e = queries[i][1], k = queries[i][2];
             for (int j = s; j <= e; j++) {
-                if (arr[j] > k && arr[j] < min) {
-                    min = arr[j];
+                if (arr[j] > k ) {
+                    min = Math.min(min, arr[j]);
                 }
             }
             answers[i] = min == Integer.MAX_VALUE ? -1 : min;

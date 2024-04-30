d5b97e0bd4b1739fa7916870db29c2c93bb94d43
anhy111 <67471703+anhy111@users.noreply.github.com>
2024-05-01 04:10:35 +0900

    [level 0] Title: 배열 만들기 2, Time: 2.25 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -6,15 +6,15 @@ class Solution {
         int start = l % 5 == 0 ? l : l + 5 - l % 5;
         for (int i = start; i <= r; i+=5) {
             boolean isZeroFive = true;
-            final String str = String.valueOf(i);
-            for (int j = 0; j < str.length(); j++) {
-                if (str.charAt(j) != '5' && str.charAt(j) != '0') {
-                    isZeroFive = false;
+            int n = i;
+            while (n > 0) {
+                if (n % 10 != 0 && n % 10 != 5) {
                     break;
                 }
+                n /= 10;
             }
 
-            if (isZeroFive) {
+            if (n == 0) {
                 list.add(i);
             }
         }
